e9ed4858e94dfb94545e50658055af80dcc03348
davicho <david@nulab.com>
2019-09-06 13:13:03 -0400

    master
     * more refactoring (js and scss)
     * enabling separate data file for languages

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ console.log("...in index.js of nu-cookie-banner");
 // require js-cookie
 // https://github.com/js-cookie/js-cookie
 const Cookies = require('js-cookie');
+const lang = require ('./languages');
 
-console.log(Cookies.get('lang'));
-let cookieAlertContainer = document.createElement('div');
-let cookieAlertBox = document.createElement('div');
-let lang = {
-  en: 'Nulab uses cookies to deliver our services. By visiting our website, you agree to the use of cookies as described in our <a href="https://nulab.com/privacy/cookie-policy/">Cookie Policy</a>.',
-  ja: "japanese"
-};
 let agree;
-let language = 'tr';
+let language = 'en';
+const cookieAlertContainer = document.createElement('div');
+cookieAlertContainer.className = 'nu-cookies-alert-container';
 
 // Check cookie (T/F)
 function cookieCheck() {
@@ -26,11 +22,7 @@ function cookieCheck() {
 
 // Get LANG cookie if set
 function getLanguageCookie(cookieLanguageKey) {
-  if (!(Cookies.get('lang'))) {
-    language = 'en';
-  } else {
-    language = Cookies.get(`${cookieLanguageKey}`);
-  }
+  language = Cookies.get(cookieLanguageKey);
 } 
 
 // Get CONSENT cookie if set
@@ -40,21 +32,20 @@ function getCookie() {
 }  
 
 // Set cookie
-function setCookie(testt) {
-  console.log(testt);
+function setCookie() {
   console.log("...setting cookie");
   Cookies.set('cookiePolicy', 'agree', { expires: 90 });
 }
 
 // Enable alert (display)
 function enableAlert() {
-  console.log("in pls show");
+  console.log("in enableAlert");
   cookieAlertContainer.style.display='flex';
 }
 
 // Disable alert
 function disableAlert() {
-  console.log("in NO show");
+  console.log("in disableAlert");
   cookieAlertContainer.style.display='none';
 }
 
@@ -85,32 +76,25 @@ document.addEventListener('click', function (event) {
 
 module.exports.render = function(cookieLanguageKey) {
   console.log("...in module.exports.render");
-
-  // get site cookie key for language if set
-  getLanguageCookie(cookieLanguageKey);
-
-  // create elements
-  let cookieAlertParagraph = document.createElement('p');
-  let cookieAlertParagraphText = lang[language];
-  let cookieAlertClose = document.createElement('div');
-  let cookieAlertCloseImg = '<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="14" cy="14" r="14" fill="#CBCBCB"/><path d="M18.6667 10C18.8508 9.81591 18.8508 9.51743 18.6667 9.33333C18.4826 9.14924 18.1841 9.14924 18 9.33333L14 13.3333L10 9.33333C9.81591 9.14924 9.51743 9.14924 9.33333 9.33333C9.14924 9.51743 9.14924 9.8159 9.33333 10L13.3333 14L9.33333 18C9.14924 18.1841 9.14924 18.4826 9.33333 18.6667C9.51743 18.8508 9.8159 18.8508 10 18.6667L14 14.6667L18 18.6667C18.1841 18.8508 18.4826 18.8508 18.6667 18.6667C18.8508 18.4826 18.8508 18.1841 18.6667 18L14.6667 14L18.6667 10Z" fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round"/></svg>';
-
-  // add classes for styling
-  cookieAlertContainer.className = 'nu-cookies-alert-container';
-  cookieAlertBox.className = 'nu-cookies-alert-box';
-  cookieAlertParagraph.className = 'nu-cookies-alert-box__text';
-  cookieAlertClose.className = 'nu-cookies-alert-box__button';
-  cookieAlertParagraph.innerHTML = cookieAlertParagraphText;
   
-  // configure elements
-  cookieAlertClose.innerHTML = cookieAlertCloseImg;
-  cookieAlertBox.appendChild(cookieAlertParagraph);
-  cookieAlertBox.appendChild(cookieAlertClose);
-  cookieAlertContainer.appendChild(cookieAlertBox);
+  // get site cookie key for language if set
+  if (Cookies.get(cookieLanguageKey)) {
+    getLanguageCookie(cookieLanguageKey);
+  }
+ 
+  let markup = `
+    <div class="nu-cookies-alert-box">
+      <p class="nu-cookies-alert-box__text">${lang[language]}</p>
+      <div class="nu-cookies-alert-box__button">
+        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="14" cy="14" r="14" fill="#CBCBCB"/><path d="M18.6667 10C18.8508 9.81591 18.8508 9.51743 18.6667 9.33333C18.4826 9.14924 18.1841 9.14924 18 9.33333L14 13.3333L10 9.33333C9.81591 9.14924 9.51743 9.14924 9.33333 9.33333C9.14924 9.51743 9.14924 9.8159 9.33333 10L13.3333 14L9.33333 18C9.14924 18.1841 9.14924 18.4826 9.33333 18.6667C9.51743 18.8508 9.8159 18.8508 10 18.6667L14 14.6667L18 18.6667C18.1841 18.8508 18.4826 18.8508 18.6667 18.6667C18.8508 18.4826 18.8508 18.1841 18.6667 18L14.6667 14L18.6667 10Z" fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round"/></svg>
+      </div>
+    </div>
+  `;
+
+  cookieAlertContainer.innerHTML = markup;
   
   // add elements to page
   document.body.appendChild(cookieAlertContainer);
 
   toggleAlert();
 }
-
